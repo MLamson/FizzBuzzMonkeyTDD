4fcb7540adead3ef9be39c8d8a2fd5b1d5e4a510
mlamson <mark_lamson@yahoo.com>
2016-07-26 12:02:12 -0400

    FizzBuzzMonekyTDD Done

---
+++ FizzBuzzMonkeyExercise/FizzBuzzMonkeyTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzzMonkeyExercise
+{
+    [TestFixture]
+    class FizzBuzzMonkeyTests
+    {
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "Fizz")]
+        [TestCase(4, "4")]
+        [TestCase(5, "Buzz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(7, "Monkey")]
+        [TestCase(10, "Buzz")]
+        [TestCase(14, "Monkey")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(3*5*7, "FizzBuzzMonkey")]
+        [TestCase(9 * 49, "FizzMonkey")]
+        [TestCase(25 * 49, "BuzzMonkey")]
+        [TestCase(9 * 25 * 49, "FizzBuzzMonkey")]
+        public void Test_1(int input, string expected)
+        {
+           var result = Program.FizzBuzzMonkey(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
---
+++ FizzBuzzMonkeyExercise/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzMonkeyExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string FizzBuzzMonkey(int input)
+        {
+            if (IsDivisibleByThreeFiveOrSeven(input))
+            {
+                var result = "";
+
+                if (IsDivisibleByThree(input))
+                    result += "Fizz";
+                if (IsDivisibleByFive(input))
+                    result += "Buzz";
+                if (IsDivisibleBySeven(input))
+                    result += "Monkey";
+
+                return result;
+            }
+            else
+            {
+                return input.ToString();
+            }
+
+        }
+
+        private static bool IsDivisibleByThreeFiveOrSeven(int input)
+        {
+            return IsDivisibleByFive(input) || IsDivisibleByThree(input) || IsDivisibleBySeven(input);
+        }
+
+        private static bool IsDivisibleBySeven(int input)
+        {
+            return input %7 == 0;
+        }
+
+        private static bool IsDivisibleByFive(int input)
+        {
+            return input %5 == 0;
+        }
+
+        private static bool IsDivisibleByThree(int input)
+        {
+            return input %3 == 0;
+        }
+    }
+}
